23f607e2e471b7a20b851f29033d0908003e7f58
MacePh <115651516+MacePh@users.noreply.github.com>
2022-12-05 11:44:38 -0500

    resolved commit conflicts

--- Address_Book/src/address_Book/Menu.java
+++ Address_Book/src/address_Book/Menu.java
@@ -1,16 +1,4 @@
-<<<<<<< HEAD
-package address_Book;
 
-
-//Claire
-public class Menu {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-	}
-
-}
-=======
 package address_Book;
 
 public class Menu {
@@ -21,4 +9,3 @@ public class Menu {
 	}
 
 }
->>>>>>> branch 'main' of https://github.com/MacePh/Java-Address-Book-Project.git
